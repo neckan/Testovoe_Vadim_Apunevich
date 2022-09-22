1cf3a79673bf25509645d6b44c6ac8acb7a43921
Neckan <neckan1999760@gmail.com>
2022-09-22 17:34:01 +0300

    dobavil commenty

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,9 +1,9 @@
 arr = ["333","1","22","4444","55555","fgeg","d","aaa","aa","dsdds"]
-index = 0
-arr2 = []
+index = 0 # schetchik indexa
+arr2 = [] # Pustoy massiv 
 while index < len(arr):
     b = arr[index]
     if  len(b) > 2:
         arr2.append(b)
-    index += 1
+    index += 1 # dvizhenie na 1 element po massivu
 print(arr2)
